5bb41c06d9d2aec6f3051057740223ecbf86c0dd
Christopher <5283833+addsandwich@users.noreply.github.com>
2022-09-24 15:35:10 -0700

    Added Sanitized Dataset

--- notebooks/data_fix.py
+++ notebooks/data_fix.py
@@ -1,4 +1,37 @@
 import re
-string = open("..\data\car_price_prediction.csv", encoding="utf8").read()
-new_str = re.sub("[^a-zA-Z0-9\n\.\,]", ' ', string)
-open('b.txt', 'w').write(new_str)
+import tkinter as tk
+from tkinter import filedialog
+
+
+def init(root):
+    root = tk.Tk()
+    root.withdraw()
+
+
+def scrub_txt_file(root):
+    # init windows stuff
+    # get the file path
+    file_path = filedialog.askopenfilename()
+    bad_string = open(file_path, encoding="utf8").read()
+    # create regex that only gets basic characters
+    regex = "[^a-zA-Z0-9\n\.\,\-]"
+    clean_str = re.sub(regex, ' ', bad_string)
+    # write out sanitized file
+    save_file(clean_str, root)
+
+
+def save_file(out_string,root):
+    fd = filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+    if fd is None: # asksaveasfile return `None` if dialog closed with "cancel".
+        return
+    fd.write(out_string)
+    fd.close() # `()` was missing.
+
+def main():
+    root = tk.Tk()
+    root.withdraw()
+    scrub_txt_file(root)
+
+
+if __name__ == "__main__":
+    main()
